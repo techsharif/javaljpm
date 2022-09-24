f1b246ae7af92caf6dcc99b1768cb81272a0ac4d
techsharif <sharif.cse.hstu@gmail.com>
2022-09-24 11:41:43 +0600

    add cast code

--- src/section03/ByteShortLong.java
+++ src/section03/ByteShortLong.java
@@ -2,10 +2,10 @@ package section03;
 
 public class ByteShortLong {
     public static void main(String[] args) {
-        byte bt1 = 127; // 8 bit
+        byte bt1 = 127; // 8 bit blu
         short st1 = 12000; // 16 bit
         int int1 = 123123123; // 32 bit
-        long lg1 = 1231231212312L; // 54 bit
+        long lg1 = 1231231212312L; // 64 bit
 
         // notice
         lg1 = 123_12312_1231_2L; // we can use _ to make it more readable
@@ -14,6 +14,12 @@ public class ByteShortLong {
         lg1 = 123_1231_23; // for small value in int range, we can ignore the ending L
 
         System.out.println(lg1 + int1 + st1);
+
+        // cast
+//        bt1 = st1 / 2; // error
+        bt1 = (byte) (st1 / 2);
+
+
     }
 }
 
