c0c0e4c93e1abfae26ad2f944d232b1ea37bf9a5
techsharif <sharif.cse.hstu@gmail.com>
2022-09-24 17:12:15 +0600

    add StringItems.java

---
+++ src/section04/StringItems.java
@@ -0,0 +1,12 @@
+package section04;
+
+public class StringItems {
+    public static void main(String[] args) {
+
+        String str = "First Line" +
+                "second line" +
+                "third line";
+
+
+    }
+}
