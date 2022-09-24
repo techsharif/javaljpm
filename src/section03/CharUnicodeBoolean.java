a08cf44a18d6299dec07971f3c301a24ad6e6fe7
techsharif <sharif.cse.hstu@gmail.com>
2022-09-24 14:22:08 +0600

    add CharUnicodeBoolean.java

---
+++ src/section03/CharUnicodeBoolean.java
@@ -0,0 +1,13 @@
+package section03;
+
+public class CharUnicodeBoolean {
+    public static void main(String[] args) {
+        char c1 = 'D';
+        char c2 = '\u0044'; // unicode value of character D
+
+        boolean a = true;
+
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
