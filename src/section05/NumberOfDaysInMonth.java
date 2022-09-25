85b5b3c7efbbdaf37b60cb1f3d33aef500659180
techsharif <sharif.cse.hstu@gmail.com>
2022-09-25 20:51:13 +0600

    Complete exercise 13

---
+++ src/section05/NumberOfDaysInMonth.java
@@ -0,0 +1,30 @@
+package section05;
+
+public class NumberOfDaysInMonth {
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) return false;
+        return (year % 400 == 0) ||
+                (year % 4 == 0 && year % 100 != 0);
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12 || year < 1 || year > 9999) return -1;
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            default:
+                return 30;
+
+        }
+    }
+}
