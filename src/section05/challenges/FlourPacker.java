5119a43c8de6701874f9eb5e1e23129d3ee65be7
techsharif <sharif.cse.hstu@gmail.com>
2022-09-26 19:50:21 +0600

    refactor code

--- src/section05/challenges/FlourPacker.java
+++ src/section05/challenges/FlourPacker.java
@@ -5,8 +5,6 @@ public class FlourPacker {
     public static boolean canPack(int bigCount, int smallCount, int goal) {
         if (bigCount < 0 || smallCount < 0 || goal < 0) return false;
 
-        int sum = bigCount * 5 + smallCount;
-
         int canPackBig = goal / 5;
         int canPackSmall = goal % 5;
 
