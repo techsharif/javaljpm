7bc2cbfc0e0d655c84a6edb9fce689be95220db5
techsharif <sharif.cse.hstu@gmail.com>
2022-09-27 16:46:43 +0600

    Create Rectangle.java to

---
+++ src/section06/Rectangle.java
@@ -0,0 +1,23 @@
+package section06;
+
+public class Rectangle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public Rectangle() {
+        this(0, 0);
+    }
+
+    public Rectangle(int width, int height) {
+        this(0, 0, width, height);
+    }
+
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
