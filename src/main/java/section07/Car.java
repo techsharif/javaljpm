b1bacefccfeb700e9fff5cc2f19e6ec4c3b5c07f
techsharif <sharif.cse.hstu@gmail.com>
2022-10-04 13:53:53 +0600

    Create basic car and vehicle class

---
+++ src/main/java/section07/Car.java
@@ -0,0 +1,29 @@
+package section07;
+
+public class Car extends Vehicle {
+
+    private int doors;
+    private int engineCapacity;
+
+    public Car(String name, int doors, int engineCapacity) {
+        super(name);
+        this.doors = doors;
+        this.engineCapacity = engineCapacity;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(int engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+}
---
+++ src/main/java/section07/Vehicle.java
@@ -0,0 +1,17 @@
+package section07;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
