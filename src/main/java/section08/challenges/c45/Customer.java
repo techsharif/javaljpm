d1de8b00143088ade7cae1706b07ee3d0a0c5e43
techsharif <sharif.cse.hstu@gmail.com>
2022-10-08 16:27:15 +0600

    Solve challenge c45 - Create basic Customer class

---
+++ src/main/java/section08/challenges/c45/Customer.java
@@ -0,0 +1,24 @@
+package section08.challenges.c45;
+
+public class Customer {
+
+    private String name;
+    private double transaction;
+
+    public Customer(String name, double transaction) {
+        this.name = name;
+        this.transaction = transaction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTransaction() {
+        return transaction;
+    }
+
+    public void addTransaction() {
+
+    }
+}
