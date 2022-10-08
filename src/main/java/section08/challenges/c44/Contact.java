b9fe654077280438cfc20f67f7611a2c98a1b486
techsharif <sharif.cse.hstu@gmail.com>
2022-10-08 16:18:48 +0600

    Solve challenge c44 - Add Contact class

---
+++ src/main/java/section08/challenges/c44/Contact.java
@@ -0,0 +1,24 @@
+package section08.challenges.c44;
+
+public class Contact {
+    // write code here
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
