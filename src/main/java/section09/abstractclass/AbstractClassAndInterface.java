f6e418efc1b1b9cc59399f2e62c7b1cbaaaf6a3f
techsharif <sharif.cse.hstu@gmail.com>
2022-10-13 20:43:47 +0600

    extend and implement both

--- src/main/java/section09/abstractclass/AbstractClassAndInterface.java
+++ src/main/java/section09/abstractclass/AbstractClassAndInterface.java
@@ -14,16 +14,33 @@ interface A {
     }
 }
 
-abstract class B implements A {
+interface AA extends A {
+    void f();
+}
+
+abstract class B {
+
+    abstract void g();
+}
+
+abstract class BB extends B implements AA {
+
+    private char ii = 'i';
+
+    abstract void h();
 
     // we don't need to add all unimplemented method
     // the methods we will not add here are still abstract methods
     public void c() { // just add the c method
         System.out.println("c");
     }
+
+    public void i() {
+        System.out.println(ii);
+    }
 }
 
-class M extends B { // need to implement all unimplemented methods
+class M extends BB { // need to implement all unimplemented methods
     public void a() {
         System.out.println("a");
     }
@@ -35,15 +52,44 @@ class M extends B { // need to implement all unimplemented methods
     public void d() {
         System.out.println("d");
     }
+
+    public void f() {
+        System.out.println("f");
+    }
+
+    public void g() {
+        System.out.println("g");
+    }
+
+    public void h() {
+        System.out.println("h");
+    }
+
+
 }
 
 public class AbstractClassAndInterface {
     public static void main(String[] args) {
-        A a = new M();
+        AA a = new M();
         a.a();
         a.b();
         a.c();
         a.d();
         a.e();
+        a.f();
+        // a.g(); // error
+        // a.h(); // error
+        // a.i()/ // error
+
+        M m = new M();
+        m.a();
+        m.b();
+        m.c();
+        m.d();
+        m.e();
+        m.f();
+        m.g();
+        m.h();
+        m.i();
     }
 }
