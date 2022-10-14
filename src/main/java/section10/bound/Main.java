344ff5eb42412cb9e18dcec65632611b6df80711
techsharif <sharif.cse.hstu@gmail.com>
2022-10-14 15:35:29 +0600

    Add bounded generics

--- src/main/java/section10/bound/Main.java
+++ src/main/java/section10/bound/Main.java
@@ -1,9 +1,20 @@
 package section10.bound;
 
+// basic
 class GenericClass1<T> {
 
     public void print(T item) {
         System.out.println(item);
+    }
+}
+
+// basic bounded
+class GenericClass2<T extends Number> {
+
+    public void print(T number) {
+        System.out.println(number.doubleValue() + 5);
+        // System.out.println(number + 5); // error -> only methods
+        // System.out.println(number + Integer.valueOf(5)); // error
     }
 }
 
@@ -13,6 +24,8 @@ public class Main {
         GenericClass1<Integer> genericClass1 = new GenericClass1<>();
         genericClass1.print(1);
 
+        GenericClass2<Integer> genericClass2 = new GenericClass2<>();
+        genericClass2.print(5);
 
     }
 }
