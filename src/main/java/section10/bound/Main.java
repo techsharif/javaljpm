56fade101e60c3ee16a23158731087b5e2654a29
techsharif <sharif.cse.hstu@gmail.com>
2022-10-14 13:45:10 +0600

    Create Basic generic class

---
+++ src/main/java/section10/bound/Main.java
@@ -0,0 +1,15 @@
+package section10.bound;
+
+class GenericClass1<T> {
+
+    public void print(T item) {
+        System.out.println(item);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
