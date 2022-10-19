4906b800f7393531e40c1390e69f8e425c6d3cb2
techsharif <sharif.cse.hstu@gmail.com>
2022-10-19 13:44:24 +0600

    Add queue interface methods on linked list

--- src/main/java/section12/linkedlist/LinkedListInterfaceMethod.java
+++ src/main/java/section12/linkedlist/LinkedListInterfaceMethod.java
@@ -1,17 +1,22 @@
 package section12.linkedlist;
 
 import section12.interfacemethods.ListInterfaceMethods;
+import section12.interfacemethods.QueueInterfaceMethods;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class LinkedListInterfaceMethod {
 
 
     public static void main(String[] args) {
-        List<Integer> arrayList = new LinkedList<>();
-        List<String> strings = new LinkedList<>();
+        LinkedList<Integer> arrayList = new LinkedList<>();
+        LinkedList<String> strings = new LinkedList<>();
         ListInterfaceMethods.show(arrayList, strings);
+
+        arrayList.clear();
+        strings.clear();
+        QueueInterfaceMethods.show(arrayList, strings);
+
 
     }
 }
