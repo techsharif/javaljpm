219c945698e311dfb8a66ebcee7b2b5c43b4c874
techsharif <sharif.cse.hstu@gmail.com>
2022-10-19 13:11:04 +0600

    Add Collection interface methods

---
+++ src/main/java/section12/interfacemethods/CollectionInterfaceMethods.java
@@ -0,0 +1,64 @@
+package section12.interfacemethods;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionInterfaceMethods {
+
+    public static void show(Collection<Integer> collection, Collection<String> strings) {
+        System.out.println("======== Collection Show " + collection.getClass().getSimpleName() + " ======== ");
+
+        // .add
+        collection.add(5);
+        collection.add(6);
+        collection.add(7);
+        System.out.println(collection); // [5, 6, 7]
+
+        // .addAll
+        collection.addAll(Arrays.asList(1, 2, 3)); // [5, 6, 7, 1, 2, 3]
+        System.out.println(collection);
+
+        // iterator()
+        Iterator<Integer> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " "); // 5 6 7 1 2 3
+        }
+        System.out.println();
+
+        strings.addAll(Arrays.asList("one", "two", "three", "one"));
+        System.out.println(strings); // [one, two, three, one]
+        strings.remove("one");
+        System.out.println(strings); // [two, three, one]
+
+        // remove All
+        collection.add(50);
+        collection.add(50);
+        collection.add(50);
+        System.out.println(collection); // [5, 6, 7, 1, 2, 3, 50, 50, 50]
+        collection.removeAll(Arrays.asList(50, 1, 3));
+        System.out.println(collection.contains(50)); // false
+        System.out.println(collection.contains(1)); // false
+        System.out.println(collection); // [5, 6, 7, 2]
+
+        strings.clear();
+        strings.addAll(Arrays.asList("one", "two", "three", "one"));
+        System.out.println(strings); // [one, two, three, one]
+        strings.removeAll(Arrays.asList("one"));
+        System.out.println(strings); // [two, three]
+
+
+        // Clear
+        System.out.println(collection); // [5, 6, 7, 2]
+        collection.clear();
+        System.out.println(collection); // []
+        System.out.println();
+
+
+    }
+
+    public static void main(String[] args) {
+        show(new ArrayList<>(), new ArrayList<>());
+    }
+}
--- src/main/java/section12/interfacemethods/ListInterfaceMethods.java
+++ src/main/java/section12/interfacemethods/ListInterfaceMethods.java
@@ -6,27 +6,20 @@ public class ListInterfaceMethods {
 
     public static void show(List<Integer> list, List<String> strings) {
 
-        System.out.println("======== Show " + list.getClass().getSimpleName() + " ======== ");
+        CollectionInterfaceMethods.show(list, strings);
+
+        System.out.println("======== List Show " + list.getClass().getSimpleName() + " ======== ");
 
         // .add
         list.add(5);
         list.add(6);
         list.add(7);
-        System.out.println(list); // [5, 6, 7]
-
-        // .addAll
-        list.addAll(Arrays.asList(1, 2, 3)); // [5, 6, 7, 1, 2, 3]
-        System.out.println(list);
+        list.add(1, 9);
+        System.out.println(list); // [5, 9, 6, 7]
 
         // get (with index)
-        System.out.println(list.get(2)); // 7
+        System.out.println(list.get(2)); // 6
 
-        // iterator()
-        Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next() + " "); // 5 6 7 1 2 3
-        }
-        System.out.println();
 
         // set()
         list.set(2, 4);
@@ -34,13 +27,13 @@ public class ListInterfaceMethods {
 
 
         // size
-        System.out.println(list.size()); // 6
+        System.out.println(list.size()); // 4
 
 
         // toArray
         Integer[] integers = new Integer[list.size()];
         list.toArray(integers);
-        System.out.println(Arrays.toString(integers)); // [5, 6, 4, 1, 2, 3]
+        System.out.println(Arrays.toString(integers)); // [5, 9, 4, 7]
 
 
         // contains
@@ -48,42 +41,15 @@ public class ListInterfaceMethods {
         System.out.println(list.contains(5)); // true
 
         // remove - single remove
-        list.set(0, 9);
+        list.set(0, 11);
         list.remove(0);
-        System.out.println(list.contains(9)); // false
+        System.out.println(list.contains(11)); // false
 
         list.set(0, 100);
         // list.remove(100); // error
         list.remove(list.get(0));
         System.out.println(list.contains(100)); // false
 
-        strings.addAll(Arrays.asList("one", "two", "three", "one"));
-        System.out.println(strings); // [one, two, three, one]
-        strings.remove("one");
-        System.out.println(strings); // [one, two, three, one]
-
-        // remove All
-        list.add(0, 50);
-        list.add(1, 50);
-        list.add(2, 50);
-        System.out.println(list); // [50, 50, 50, 4, 1, 2, 3]
-        list.removeAll(Arrays.asList(50, 1, 3));
-        System.out.println(list.contains(50)); // false
-        System.out.println(list.contains(1)); // false
-        System.out.println(list); // [4, 2]
-
-        strings.clear();
-        strings.addAll(Arrays.asList("one", "two", "three", "one"));
-        System.out.println(strings); // [one, two, three, one]
-        strings.removeAll(Arrays.asList("one"));
-        System.out.println(strings); // [two, three]
-
-
-        // Clear
-        System.out.println(list); // [4, 2]
-        list.clear();
-        System.out.println(list); // []
-        System.out.println();
 
     }
 
