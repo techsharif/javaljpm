be4af0b9d44a7f50e938493721038f24f53618f7
techsharif <sharif.cse.hstu@gmail.com>
2022-10-21 15:26:40 +0600

    Add arraydeque class method

---
+++ src/main/java/section12/arraydeque/ArrayDequeClassMethod.java
@@ -0,0 +1,11 @@
+package section12.arraydeque;
+
+import section12.interfacemethods.DequeInterfaceMethods;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeClassMethod {
+    public static void main(String[] args) {
+        DequeInterfaceMethods.show(new ArrayDeque<>(), new ArrayDeque<>());
+    }
+}
