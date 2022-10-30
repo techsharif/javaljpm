71a4476703fa5bc0a1acf41330b670814ee9b827
techsharif <sharif.cse.hstu@gmail.com>
2022-10-30 09:32:37 +0600

    Add HashSet class

---
+++ src/main/java/section12/set/HashSetClassMethods.java
@@ -0,0 +1,12 @@
+package section12.set;
+
+import section12.interfacemethods.SetInterfaceMethods;
+
+import java.util.HashSet;
+
+public class HashSetClassMethods {
+
+    public static void main(String[] args) {
+        SetInterfaceMethods.show(new HashSet<>(), new HashSet<>());
+    }
+}
