e29db0bea1277dec542278ccb887a54c9398a450
techsharif <sharif.cse.hstu@gmail.com>
2022-10-30 09:37:40 +0600

    Add LinkHashSetClassMethods.java

---
+++ src/main/java/section12/set/LinkHashSetClassMethods.java
@@ -0,0 +1,12 @@
+package section12.set;
+
+import section12.interfacemethods.SetInterfaceMethods;
+
+import java.util.LinkedHashSet;
+
+public class LinkHashSetClassMethods {
+
+    public static void main(String[] args) {
+        SetInterfaceMethods.show(new LinkedHashSet<>(), new LinkedHashSet<>());
+    }
+}
