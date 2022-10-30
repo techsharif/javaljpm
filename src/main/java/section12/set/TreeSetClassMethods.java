f59f8df125d22e1abeb65b221af2bb45e67c6b9e
techsharif <sharif.cse.hstu@gmail.com>
2022-10-30 10:30:08 +0600

    Add TreeSetClassMethods.java

---
+++ src/main/java/section12/set/TreeSetClassMethods.java
@@ -0,0 +1,12 @@
+package section12.set;
+
+import section12.interfacemethods.NavigableSetInterfaceMethods;
+
+import java.util.TreeSet;
+
+public class TreeSetClassMethods {
+
+    public static void main(String[] args) {
+        NavigableSetInterfaceMethods.show(new TreeSet<>(), new TreeSet<>());
+    }
+}
